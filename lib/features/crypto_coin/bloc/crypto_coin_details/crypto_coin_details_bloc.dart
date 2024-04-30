650b1867581fcc28738f0dc7491808416cfa83b9
NiksKozlov <nikskozlov@gmail.com>
2024-04-30 22:34:26 +0300

    Add talker dio logger and talker block logger; Talker initialized;  Add TalkerRouteObserver; Logging of unhandled errors is configured

--- lib/features/crypto_coin/bloc/crypto_coin_details/crypto_coin_details_bloc.dart
+++ lib/features/crypto_coin/bloc/crypto_coin_details/crypto_coin_details_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 part 'crypto_coin_details_event.dart';
 part 'crypto_coin_details_state.dart';
@@ -26,8 +28,16 @@ class CryptoCoinDetailsBloc
       final coinDetails = await coinsRepository.getCoinDetails(event.currencyCode);
 
       emit(CryptoCoinDetailsLoaded(coinDetails));
-    } catch (e) {
-      emit(CryptoCoinDetailsLoadingFailure(e));
+    } catch (error, stackTrace) {
+      emit(CryptoCoinDetailsLoadingFailure(error));
+      GetIt.I<Talker>().handle(error, stackTrace);
+
     }
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    super.onError(error, stackTrace);
+    GetIt.I<Talker>().handle(error, stackTrace);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,36 @@
+import 'dart:async';
+
 import 'package:crypto_coins_list/crypto_currencies_list_app.dart';
 import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get_it/get_it.dart';
+import 'package:talker_dio_logger/talker_dio_logger.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 void main() {
+  final talker = TalkerFlutter.init();
+
+  GetIt.I.registerSingleton(talker);
+
+  final dio = Dio();
+  dio.interceptors.add(TalkerDioLogger(
+    talker: talker,
+    settings: const TalkerDioLoggerSettings(
+      printResponseData: false
+    )
+  ));
+
   GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
-    () => CryptoCoinsRepository(dio: Dio())
+    () => CryptoCoinsRepository(dio: dio)
   );
-  runApp(const CryptoCurrenciesListApp());
+
+  FlutterError.onError =
+      (details) => GetIt.I<Talker>().handle(details.exception, details.stack);
+
+  runZonedGuarded(
+    () => runApp(const CryptoCurrenciesListApp()),
+    (error, stack) => GetIt.I<Talker>().handle(error, stack)
+  );
 }

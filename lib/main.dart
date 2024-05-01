ab10c70c9483a65c85335ff8f8ba8e93dd7bc7d0
NiksKozlov <nikskozlov@gmail.com>
2024-05-01 20:03:44 +0300

    Add bloc talker observer to main; Add button to appbar for open talker monitor

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger.dart';
 import 'package:talker_dio_logger/talker_dio_logger.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
@@ -21,6 +23,14 @@ void main() {
       printResponseData: false
     )
   ));
+
+  Bloc.observer = TalkerBlocObserver(
+    talker: talker,
+    settings: const TalkerBlocLoggerSettings(
+      printStateFullData: false,
+      printEventFullData: false
+    )
+  );
 
   GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
     () => CryptoCoinsRepository(dio: dio)
